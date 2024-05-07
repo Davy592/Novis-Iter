18a57496e8fe94719c2e9df0525ed2a4b03e1907
Davide Cirilli <davide.cirilli592@gmail.com>
2024-05-07 20:01:15 +0200

    Migliorato movimento e cambiato sprite protagonista

--- player.gd
+++ player.gd
@@ -1,42 +1,77 @@
 extends CharacterBody2D
 
-var speed = 400
-var angular_speed = PI
+var MAX_SPEED = 500
+var ACCELERATION = 2000000000
+var dir = "down"
+var axis
 
-var current_dir = "None"
+func _physics_process(delta):
+	axis = get_input_axis()
+	if axis == Vector2.ZERO:
+		apply_friction(ACCELERATION * delta)
+		play_anim(false)
+	else:
+		apply_movement(axis * ACCELERATION * delta)
+		play_anim(true)
+	move_and_slide()
 
-func _process(delta):
-	var velocity = Vector2.ZERO
-	
-	if Input.is_action_pressed("ui_left"):
-		play_anim(4)
-		velocity += Vector2.LEFT * speed
-	elif Input.is_action_pressed("ui_right"):
-		play_anim(3)
-		velocity += Vector2.RIGHT * speed
-	if Input.is_action_pressed("ui_up"):
-		play_anim(1)
-		velocity += Vector2.UP * speed
-	elif Input.is_action_pressed("ui_down"):
-		play_anim(2)
-		velocity += Vector2.DOWN * speed
 
-	position += velocity * delta
+func get_input_axis():
+	var axis = Vector2.ZERO
+	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	if axis.y > 0:
+		dir = "down"
+	elif axis.y < 0:
+		dir = "up"
+	elif axis.x > 0:
+		dir = "right"
+	elif axis.x < 0:
+		dir = "left"
+	return axis.normalized()
+
+
+func apply_friction(deceleration):
+	if velocity.x < 0:
+		velocity.x += deceleration
+		velocity.x = clamp(velocity.x, -MAX_SPEED, 0)
+	else:
+		velocity.x -= deceleration
+		velocity.x = clamp(velocity.x, 0, MAX_SPEED)
+	if velocity.y < 0:
+		velocity.y += deceleration
+		velocity.y = clamp(velocity.y, -MAX_SPEED, 0)
+	else:
+		velocity.y -= deceleration
+		velocity.y = clamp(velocity.y, 0, MAX_SPEED)
+	print(velocity)
+
+
+func apply_movement(acceleration):
+	velocity = Vector2.ZERO
+	velocity += acceleration
+	velocity = velocity.clamp(Vector2(-MAX_SPEED, -MAX_SPEED), Vector2(MAX_SPEED, MAX_SPEED))
+	#velocity = velocity.normalized() * MAX_SPEED
+	print(velocity)
+
 
 func play_anim(movement):
-	var dir = current_dir
 	var anim = $AnimatedSprite2D
-	
-	match movement:
-		1:
-			anim.flip_h=false
-			anim.play("back_walk")
-		2:
-			anim.flip_h=false
+	if movement:
+		if dir == "down":
 			anim.play("front_walk")
-		3:
-			anim.flip_h=false
-			anim.play("side_walk")
-		4:
-			anim.flip_h=true
-			anim.play("side_walk")
+		elif dir == "up":
+			anim.play("back_walk")
+		elif dir == "right":
+			anim.play("right_walk")
+		elif dir == "left":
+			anim.play("left_walk")
+	else:
+		if dir == "down":
+			anim.play("front_idle")
+		elif dir == "up":
+			anim.play("back_idle")
+		elif dir == "right":
+			anim.play("right_idle")
+		elif dir == "left":
+			anim.play("left_idle")
