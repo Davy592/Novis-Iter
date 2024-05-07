416942e7c4f2ea65b452a99159dbf6c776b7c82f
ashkihotah <nicoloresta02@gmail.com>
2024-05-07 22:31:37 +0200

    Aggiustato e pulito lo script di movimento del player

--- player.gd
+++ player.gd
@@ -1,77 +1,53 @@
 extends CharacterBody2D
 
-var MAX_SPEED = 500
-var ACCELERATION = 2000000000
-var dir = "down"
-var axis
+var MAX_SPEED = 600
+var ACCELERATION = 150
+var DECELERATION = ACCELERATION * 0.3
+var direction = "front"
 
 func _physics_process(delta):
-	axis = get_input_axis()
-	if axis == Vector2.ZERO:
-		apply_friction(ACCELERATION * delta)
-		play_anim(false)
-	else:
-		apply_movement(axis * ACCELERATION * delta)
-		play_anim(true)
-	move_and_slide()
-
-
-func get_input_axis():
 	var axis = Vector2.ZERO
-	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	axis.y = int(Input.is_action_pressed("move_front")) - int(Input.is_action_pressed("move_back"))
+	axis = axis.normalized()
+	velocity += axis * ACCELERATION
+	#velocity *= 0.8
+	#velocity -= velocity * 0.1
+	#velocity -= velocity.normalized() * DECELERATION
+	#if velocity.length() > MAX_SPEED:
+		#velocity = velocity.normalized() * MAX_SPEED
+	apply_friction()
+	move_and_slide()
+	play_animation(axis)
+	print(velocity)
+
+func play_animation(axis):
+	var anim = $AnimatedSprite2D
 	if axis.y > 0:
-		dir = "down"
+		anim.play("front_walk")
+		direction = "front"
 	elif axis.y < 0:
-		dir = "up"
+		anim.play("back_walk")
+		direction = "back"
 	elif axis.x > 0:
-		dir = "right"
+		anim.play("right_walk")
+		direction = "right"
 	elif axis.x < 0:
-		dir = "left"
-	return axis.normalized()
+		anim.play("left_walk")
+		direction = "left"
+	else:
+		anim.play(direction + "_idle")
 
-
-func apply_friction(deceleration):
+func apply_friction():
 	if velocity.x < 0:
-		velocity.x += deceleration
+		velocity.x += DECELERATION
 		velocity.x = clamp(velocity.x, -MAX_SPEED, 0)
 	else:
-		velocity.x -= deceleration
+		velocity.x -= DECELERATION
 		velocity.x = clamp(velocity.x, 0, MAX_SPEED)
 	if velocity.y < 0:
-		velocity.y += deceleration
+		velocity.y += DECELERATION
 		velocity.y = clamp(velocity.y, -MAX_SPEED, 0)
 	else:
-		velocity.y -= deceleration
+		velocity.y -= DECELERATION
 		velocity.y = clamp(velocity.y, 0, MAX_SPEED)
-	print(velocity)
-
-
-func apply_movement(acceleration):
-	velocity = Vector2.ZERO
-	velocity += acceleration
-	velocity = velocity.clamp(Vector2(-MAX_SPEED, -MAX_SPEED), Vector2(MAX_SPEED, MAX_SPEED))
-	#velocity = velocity.normalized() * MAX_SPEED
-	print(velocity)
-
-
-func play_anim(movement):
-	var anim = $AnimatedSprite2D
-	if movement:
-		if dir == "down":
-			anim.play("front_walk")
-		elif dir == "up":
-			anim.play("back_walk")
-		elif dir == "right":
-			anim.play("right_walk")
-		elif dir == "left":
-			anim.play("left_walk")
-	else:
-		if dir == "down":
-			anim.play("front_idle")
-		elif dir == "up":
-			anim.play("back_idle")
-		elif dir == "right":
-			anim.play("right_idle")
-		elif dir == "left":
-			anim.play("left_idle")
