fcb92c21804e3bbff854580af955413710d15bb8
ashkihotah <nicoloresta02@gmail.com>
2024-05-20 13:27:02 +0200

    rimosse righe di codice inutili

--- actionable.gd
+++ actionable.gd
@@ -1,9 +1,6 @@
 extends Area2D
 
-signal action_is_ended
-
 func action() -> void:
 	print('Action Executed!')
 	# Esempio di azione
 	Dialogic.start('res://timelines/prova.dtl')
-	emit_signal("action_is_ended")
