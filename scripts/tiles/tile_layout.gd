53d9ac5a6aed68f280c9d1967014ed057b34da2f
ashkihotah <nicoloresta02@gmail.com>
2024-05-30 18:17:35 +0200

    aggiunto il codice di cambio tile e adattato il progetto per tale fine

--- scripts/tiles/tile_layout.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _ready():
-	$Player.menu_requested.connect($CanvasLayer/InGameMenu._on_player_menu_requested)
-	$Player.inventory_requested.connect($CanvasLayer/InventoryUI._on_player_inventory_requested)
-	$Player.interact_ui_requested.connect($CanvasLayer/InteractUI._on_player_interact_ui_requested)
-	$Player.interact_ui_revoked.connect($CanvasLayer/InteractUI._on_player_interact_ui_revoked)
