f90313ad2d08b2c95cbf9f7979d0ee7550b83883
ashkihotah <nicoloresta02@gmail.com>
2024-06-08 12:01:12 +0200

    Aggiunto codice per il setting dei limiti di cam di default a inizio gioco

--- scripts/main.gd
+++ scripts/main.gd
@@ -11,3 +11,8 @@ func _ready():
 	$Player.position = tile_info.get_default_entry_point()
 	Global.current_map_node_updated.connect($CanvasLayer/MapEditor._on_current_map_node_updated)
 	
+	var camera: Camera2D = $Player.get_node('Camera2D')
+	camera.limit_top = tile_info.get_cam_top_limit()
+	camera.limit_right = tile_info.get_cam_right_limit()
+	camera.limit_bottom = tile_info.get_cam_bottom_limit()
+	camera.limit_left = tile_info.get_cam_left_limit()
