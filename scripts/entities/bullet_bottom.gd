72cf9409fe13c86221d8c2d96c797778b025841d
SaraPadovano <padovano.sara23@gmail.com>
2024-06-19 10:59:57 +0200

    aggiunti i signal per la penalità del player quando viene colpito da un proiettile

--- scripts/entities/bullet_bottom.gd
+++ scripts/entities/bullet_bottom.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+var explosion = false
+var speed = 70
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +16,14 @@ func _process(delta):
 
 func _on_area_2d_area_entered(area):
 	$AnimatedSprite2D.play("bullet_explosion")
+	explosion = true
+
+
+func _on_area_2d_body_entered(body):
+	if  not explosion:
+		if body.has_signal("map_requested"):
+			$AnimatedSprite2D.play("bullet_explosion")
+			if body.position.y < 1030:
+				body.position.y += speed
+				
+			
--- scripts/entities/bullet_top.gd
+++ scripts/entities/bullet_top.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var explosion = false
+var speed = 70
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +16,12 @@ func _process(delta):
 
 func _on_area_2d_area_entered(area):
 	$AnimatedSprite2D.play("bullet_explosion")
+	explosion = true
+
+
+func _on_area_2d_body_entered(body):
+	if  not explosion:
+		if body.has_signal("map_requested"):
+			$AnimatedSprite2D.play("bullet_explosion")
+			if body.position.y > -2:
+				body.position.y -= speed
