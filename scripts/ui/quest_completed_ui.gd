f90872f9570b95838390f1004c5bde5525a6d38a
Giuseppe Tedone <giuseppe.tedone@hevolus.it>
2024-06-20 11:44:45 +0200

    Reso il titolo della quest completata dinamico

--- scripts/ui/quest_completed_ui.gd
+++ scripts/ui/quest_completed_ui.gd
@@ -1,14 +1,17 @@
 extends Control
 
 var color_rect : ColorRect
+var label : Label
 var timer : Timer
 
 func _ready():
 	Global.quest_handler.quest_completed.connect(_on_quest_completed)
 	color_rect = $ColorRect
+	label = $ColorRect/Label
 	timer = $Timer
 	
 func _on_quest_completed(quest_title: String):
+	label.text = "Quest \"" + quest_title + "\" completata!" 
 	visible = true
 	timer.start()
 
