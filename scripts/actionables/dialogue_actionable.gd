ee33c929858096a890943b6a6781ff5df7effbc1
Giuseppe Tedone <giuseppe.tedone@hevolus.it>
2024-06-20 11:50:02 +0200

    Aggiunti tipi delle variabili
    Aggiunte code regions per migliore leggibilita'
    
    Aggiunti tipi di ritorno delle funzioni

--- scripts/actionables/dialogue_actionable.gd
+++ scripts/actionables/dialogue_actionable.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
-@export var dialogue_resource: DialogicTimeline
+@export var dialogue_resource: DialogicTimeline:
+	set(new_dialog): 
+		dialogue_resource = new_dialog
+	get: return dialogue_resource
 
 func action() -> void:
 	Dialogic.start(dialogue_resource)
--- scripts/actionables/item_actionable.gd
+++ scripts/actionables/item_actionable.gd
@@ -4,15 +4,21 @@ extends Node2D
 @export_file("*.json") var json_data_file
 var item: Item
 
+## Istanzia l'item, acquisendo i dati corrispondenti dal file 
+## JSON specificato, e assegna la texture al nodo "Sprite2D".
 func _ready():
 	var json_as_text = FileAccess.get_file_as_string(json_data_file)
 	var item_data = JSON.parse_string(json_as_text)
+	
 	item_data['quantity'] = item_quantity
 	item_data['texture'] = load(item_data['texture'])
 	item = Item.new(item_data)
+	
 	if not Engine.is_editor_hint():
 		$Sprite2D.texture = item_data['texture']
 
+## Aggiunge l'item all'inventario e aggiorna le quest interessate.
 func action():
 	Global.inventory.add(item)
+	Global.quest_handler.update_quests()
 	self.queue_free()
