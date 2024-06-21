49b93b05d506da2f1c02d04b3b58625d472dcb4d
SaraPadovano <padovano.sara23@gmail.com>
2024-06-21 12:16:04 +0200

    scrivendo il codice per far seguire il player da remy

--- scripts/entities/remy.gd
+++ scripts/entities/remy.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var player_position
+var target_position
+var MAX_SPEED = 600
+var ACCELERATION = 150
+var DECELERATION = 0.2
+@export var player = get_parent().get_node("Player")
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -10,26 +14,16 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready():
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func movement():
+	pass
+	
 
 func _on_dialogic_signal(argument:String):
 	if argument == "follower":
 		print("Something was activated!")
+		movement()
+		
+
+
+func _on_timer_timeout():
+	pass # Replace with function body.
