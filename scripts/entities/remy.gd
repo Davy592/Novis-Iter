f3c8e1dc1d597b08da3c2548e2169d65f0709b6c
SaraPadovano <padovano.sara23@gmail.com>
2024-06-22 12:20:24 +0200

    completato il codice per far seguire il player da remy per un determinato lasso di tempo

--- scripts/entities/remy.gd
+++ scripts/entities/remy.gd
@@ -1,12 +1,10 @@
 extends CharacterBody2D
 
-const JUMP_VELOCITY = -400.0
-var player_position
-var target_position
-var MAX_SPEED = 600
 var ACCELERATION = 150
-var DECELERATION = 0.2
-@export var player = get_parent().get_node("Player")
+@export var target_character: CharacterBody2D
+@onready var navigation_agent = $NavigationAgent2D
+var move = false
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,16 +12,45 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready():
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 
-func movement():
-	pass
-	
+func _physics_process(delta):
+	if move:
+		var distance_to_target = target_character.position.distance_to(position)
+		set_movement_target(target_character.global_position)
+		if navigation_agent.is_navigation_finished() or distance_to_target < 100:
+			return
+		var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+		var current_agent_position: Vector2 = global_position
+		var new_velocity: Vector2 = (next_path_position - current_agent_position).normalized() * ACCELERATION * 2
+		velocity = new_velocity  
+		move_and_slide() 
+		handle_animation(velocity)
+
+func handle_animation(velocity: Vector2):
+	if velocity.length_squared() > 0:
+		if abs(velocity.x) > abs(velocity.y):
+			if velocity.x > 0:
+				$AnimatedSprite2D.play("right_walk")
+			else:
+				$AnimatedSprite2D.play("left_walk")
+		else:
+			if velocity.y > 0:
+				$AnimatedSprite2D.play("front_walk")
+			else:
+				$AnimatedSprite2D.play("back_walk")
+	else:
+		$AnimatedSprite2D.stop()
+
+func set_movement_target(movement_target: Vector2):
+	navigation_agent.set_target_position(movement_target)
 
 func _on_dialogic_signal(argument:String):
 	if argument == "follower":
 		print("Something was activated!")
-		movement()
-		
+		move = true
+		$Timer.start()
+		$CollisionShape2D.disabled = true
 
 
 func _on_timer_timeout():
-	pass # Replace with function body.
+	move = false
+	$CollisionShape2D.disabled = false
