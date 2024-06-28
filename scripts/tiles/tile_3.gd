d4d67c977191ba81fe4ca6ebe2405a5080551e3d
SaraPadovano <padovano.sara23@gmail.com>
2024-06-28 21:59:56 +0200

    modificato tile3 per consentire il completo nascondiglio della creatura del tile

--- scripts/tiles/tile_3.gd
+++ scripts/tiles/tile_3.gd
@@ -3,6 +3,8 @@ extends Node2D
 var creature = false
 var path_together = false
 var path_different = false
+var tile3_creature_scene = preload("res://scenes/entities/tile_3_creature.tscn")
+var creature_node: Node2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,12 +19,15 @@ func _process(delta):
 func _on_dialogic_signal(argument:String):
 	if argument == "together":
 		creature = true
-		$tile3_creature.visible = true
 		Dialogic.VAR.creature_action = true
 		Dialogic.VAR.quest = true
 		Dialogic.VAR.husband = false
+		creature_node = tile3_creature_scene.instantiate()
+		creature_node.position.x = 730
+		creature_node.position.y = 988
+		get_parent().add_child(creature_node)
 	if argument == "death":
-		$tile3_creature.rotation_degrees = -90
+		creature_node.rotation_degrees = -90
 	if argument == "path_together":
 		$Timer1.start()
 		path_together = true
@@ -37,9 +42,10 @@ func _on_dialogic_signal(argument:String):
 func _on_timer_1_timeout():
 	if path_together:
 		$grisby.visible = false
-		$remy_example.visible = false
+		#remy visibilità
 	if path_different:
-		$remy_example.visible = false
+		pass
+		#remy visivilità
 
 
 func _on_timer_2_timeout():
